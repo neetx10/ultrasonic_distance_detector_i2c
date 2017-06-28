ad29ecf8713998b48349958f4871e931614f8a84
Navdeep Soni <navdeep.soni@aurigait.com>
2017-06-28 22:59:10 +0530

    added burger with distance.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 
+import com.pi4j.gpio.extension.mcp.MCP23017GpioProvider;
+import com.pi4j.gpio.extension.mcp.MCP23017Pin;
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CFactory;
+
+import java.io.IOException;
 
 
 public class Main {
@@ -10,13 +16,16 @@ public class Main {
 
     final static GpioController gpio = GpioFactory.getInstance();
 
-    public static void main(String [] args) throws InterruptedException{
+    public static void main(String [] args) throws InterruptedException, IOException, I2CFactory.UnsupportedBusNumberException {
         new Main().run();
     }
-    public void run() throws InterruptedException{
+    public void run() throws InterruptedException, IOException, I2CFactory.UnsupportedBusNumberException {
+
+        final MCP23017GpioProvider provider = new MCP23017GpioProvider(I2CBus.BUS_1, 0x20);
+
         sensorTriggerPin =  gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00); // Trigger pin as OUTPUT
         sensorEchoPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02,PinPullResistance.PULL_DOWN); // Echo pin as INPUT
-
+        final  GpioPinDigitalOutput burgerPin = gpio.provisionDigitalOutputPin(provider, MCP23017Pin.GPIO_A0,"A0");
         while(true){
             try {
                 Thread.sleep(500);
@@ -33,7 +42,11 @@ public class Main {
                 }
                 long endTime= System.nanoTime(); // Store the echo pin HIGH end time to calculate ECHO pin HIGH time.
 
-                System.out.println("Distance :"+((((endTime-startTime)/1e3)/2) / 29.1) +" cm"); //Printing out the distance in cm
+                double distance = ((((endTime-startTime)/1e3)/2) / 29.1);
+                System.out.println("Distance :"+ distance +" cm"); //Printing out the distance in cm
+
+                if(distance<30)  burgerPin.high();
+                else burgerPin.low();
                 Thread.sleep(500);
 
             } catch (InterruptedException e) {
